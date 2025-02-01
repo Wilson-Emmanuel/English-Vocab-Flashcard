6bc150134228a7c8aff95fa02d748e8e7dc9728e
wilson <wilson@anymindgroup.com>
2025-02-01 12:45:48 +0100

    initial commit

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'v1';
+const urlsToCache = [
+  'https://unpkg.com/@tailwindcss/browser@4'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
